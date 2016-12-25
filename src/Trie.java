2a030da229b5be446caae182fa47a5b2356ddf07
Vinija Jain <vinija@gmail.com>
2016-12-25 14:44:14 -0500

    Duran Duran

--- src/Trie.java
+++ src/Trie.java
@@ -1,9 +1,60 @@
+import java.util.HashMap;
+
 /**
  * Created by vinija on 12/25/16.
  */
 public class Trie {
+    private TrieNode root;
 
-    int prefix;
-    int words;
-    int [] edges = new int[26];
+    public Trie(){
+        root = new TrieNode();
+    }
+
+    //Insert a word into the Trie
+    public void insert(String word){
+        HashMap<Character,TrieNode> children = root.children;
+
+        for(int i = 0; i<word.length(); i++){
+            char c = word.charAt(i);
+
+            TrieNode t;
+
+            if(children.containsKey(c)){
+                t = children.get(c);
+            }else{
+                t = new TrieNode(c);
+                //Character, TrieNode
+                children.put(c,t);
+            }
+
+            children = t.children;
+
+            if(i==word.length()-1)
+                t.isLeaf = true;
+        }
+    }
+
+    public boolean search (String word){
+        TrieNode t = searchNode(word);
+        if(t != null && t.isLeaf)
+            return true;
+        else
+            return false;
+
+    }
+
+    public TrieNode searchNode(String word){
+        HashMap<Character, TrieNode> children = root.children;
+        TrieNode t = null;
+        for (int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(children.containsKey(c)){
+                t= children.get(c);
+                children = t.children;
+            }else{
+                return null;
+            }
+        }
+        return t;
+    }
 }
--- src/TrieNode.java
+++ src/TrieNode.java
@@ -1,5 +1,18 @@
+import java.util.HashMap;
+
 /**
  * Created by vinija on 12/25/16.
  */
 public class TrieNode {
+
+    char c;
+    HashMap<Character,TrieNode> children = new HashMap<Character,TrieNode>();
+    boolean isLeaf;
+
+    public TrieNode(char c){
+        this.c = c;
+    }
+
+    public TrieNode(){}
+
 }
